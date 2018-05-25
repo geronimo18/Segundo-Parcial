798cf571d3d6bf9fa46b7e7e3ce7527c8ff4832b
Estudiantes <Estudiantes@10.20.151.195>
2018-05-25 12:02:01 -0500

    Clases definidas.

--- segundoparcial/src/segundoparcial/Principal.java
+++ segundoparcial/src/segundoparcial/Principal.java
@@ -3,7 +3,8 @@ package segundoparcial;
 public class Principal {
 
     public static void main(String[] args) {
-      
+        Calculador miCalculador; 
+        miCalculdor = new Calculador();
     }
     
 }
